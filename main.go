c5d9c55e70207ba0df29d06e9f0b8a5d7df357c5
Kevin Park <parkkevin100@gmail.com>
2019-01-13 15:30:51 -0800

    Initial commit

---
+++ main.go
@@ -0,0 +1,28 @@
+// Kevin Park
+// 301322108
+
+package main
+
+import (
+	"blockchain"
+	"fmt"
+)
+
+func main() {
+
+	b0 := blockchain.Initial(7)
+	b0.Mine(1)
+	b := blockchain.Blockchain{}
+	b.Add(b0)
+	b1 := b0.Next("this is an interesting message")
+	b1.Mine(1)
+	b.Add(b1)
+	b2 := b1.Next("this is not interesting")
+	b2.Mine(1)
+	b.Add(b2)
+	if b.IsValid() {
+		fmt.Printf("The blockchain is valid: %t\n", true)
+	} else {
+		fmt.Printf("The blockchain in valid: %t\n", false)
+	}
+}
---
+++ work_queue.go
@@ -0,0 +1,39 @@
+// Kevin Park
+// 301322108
+
+package work_queue
+
+type Worker interface {
+	Run() interface{}
+}
+
+type WorkQueue struct {
+	Jobs    chan Worker
+	Results chan interface{}
+}
+
+func Create(nWorkers uint, maxJobs uint) *WorkQueue {
+	q := new(WorkQueue)
+	// TODO: initialize struct; start nWorkers workers as goroutines
+	q.Jobs = make(chan Worker, maxJobs)
+	q.Results = make(chan interface{}, maxJobs)
+	for i := uint(0); i < nWorkers; i++ {
+		go q.worker()
+	}
+	return q
+}
+
+func (queue WorkQueue) worker() {
+	for job := range queue.Jobs {
+		queue.Results <- job.Run()
+	}
+}
+
+func (queue WorkQueue) Enqueue(work Worker) {
+	queue.Jobs <- work
+}
+
+func (queue WorkQueue) Shutdown() {
+	close(queue.Jobs)
+	for _ = range queue.Jobs {}
+}
